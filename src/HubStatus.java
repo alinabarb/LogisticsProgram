f465ac977e7beded446d7d7ed1dc1cbe45c18344
alinabarik <alina.barbashova@gmail.com>
2026-07-21 19:57:50 +0300

    Инициализированы классы HubStatus и LogisticHub

---
+++ src/HubStatus.java
@@ -0,0 +1,3 @@
+public enum HubStatus {
+    OPEN, CLOSE
+}
---
+++ src/LogisticHub.java
@@ -0,0 +1,5 @@
+public class LogisticHub {
+    int maxCapacity = 10000;
+    double weight = 0.0;
+    HubStatus status = HubStatus.OPEN;
+}
