f74bb8b7f9b66b3714c34f180ccdd9de4972019b
alinabarik <alina.barbashova@gmail.com>
2026-07-22 22:53:04 +0300

    Добавлена возможность вводить название груза и вес, а также вывод названия груза, его веса и доступного остатка для груза

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите название груза:");
+        String product = scanner.nextLine();
+        System.out.print("Введите вес груза;");
+        double weight = scanner.nextDouble();
+        double hubCapacity = 10000.0;
+        double balance =  hubCapacity - weight;
+        System.out.printf("Груз %s успешно зарегистрирован. Текущий вес: %.2f, осталось свободного места: %.2f ", product, weight, balance);
     }
 }
