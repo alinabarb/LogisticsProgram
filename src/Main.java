07f8802f5f4d51820d78229f6071748e52a8c1f9
alinabarik <alina.barbashova@gmail.com>
2026-07-25 22:33:11 +0300

    добавила запись продуктов в массив и выход если массив заполнен

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
         String product;
         double weight;
         String answer = "Y";
+        String[] journal = new String[5];
+        int product_id = 0;
         Scanner scanner = new Scanner(System.in);
-        while(answer.equals("Y")){
+        while(answer.equals("Y") ){
             System.out.print("Введите название груза:");
             product = scanner.nextLine();
             System.out.print("Введите вес груза:");
@@ -15,15 +17,26 @@ public class Main {
             balance -= weight;
             if(balance>=0) {
                 System.out.printf("Груз %s успешно зарегистрирован. Текущий вес: %.2f, осталось свободного места: %.2f ", product, weight, balance);
+                journal[product_id] = product;
+                product_id++;
             }
             else
             {
                 balance += weight;
                 System.out.printf("Груз %s не зарегистрирован по причине превышения веса. Осталось свободного места: %.2f ", product, balance);
             }
-
-            System.out.print("\nПродолжить регистрацию? (Y/N)");
-            answer = scanner.nextLine();
+            if(product_id<5) {
+                System.out.print("\nПродолжить регистрацию? (Y/N)");
+                answer = scanner.nextLine();
+            }
+            else answer = "N";
         }
+        System.out.println("\nИтоговый список товаров:");
+        for(String products:journal) {
+            if(products!=null) {
+                System.out.println(products);
+            }
+        }
+
     }
 }
